00ea57f9d43ef36c7a3a2a293bef5574e19f8ddf
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2019-10-08 10:46:11 +0200

    Created new Folder L02, added a C# basic Setup

---
+++ L02_C#/Obj1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace L02_C_
+{
+    class Obj1
+    {
+        public int i = 42;
+        public double pi = 3.1415;
+        public string salute = "Hello, World - i am Daniel";
+    }
+}
---
+++ L02_C#/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace L02_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Obj1 obj = new Obj1();
+            Console.WriteLine(obj.salute);
+        }
+    }
+}
