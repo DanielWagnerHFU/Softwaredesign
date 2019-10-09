a471e40018a9da380282b9b482d08baaea9897fe
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2019-10-09 17:57:51 +0200

    implemented reverseWords

--- A01_Buchstabendreher/Program.cs
+++ A01_Buchstabendreher/Program.cs
@@ -9,9 +9,9 @@ namespace A01_Buchstabendreher
             Console.WriteLine("Bitte einen kleinen Satz eingeben");
             Console.Write("> ");
             var text = Console.ReadLine();
-            string letters = reverseLetters(text);
+            //string letters = reverseLetters(text);
             string words = reverseWords(text);
-            string sentence = reverseSentence(text);
+            //string sentence = reverseSentence(text);
             Console.WriteLine(sentence + "\n" + words + "\n" + letters);
         }
         //reverses the Letters of a String (shortest possible Implementation)
@@ -21,15 +21,29 @@ namespace A01_Buchstabendreher
         }
         public static String reverseWords(String text)
         {
-            return text;
+            if (text.Length == 0){
+                return "";
+            } else {
+                String [] words = text.Split(' ');
+                String [] restStrings = SubArray(words, 0, words.Length - 1);
+                String lastString = words[words.Length - 1];
+                String newText = String.Join(" ", restStrings);
+                newText = lastString + newText;
+                return newText;
+            }
+        }
+        public static String reverseWordsHelper(String[] words){
+            return null;
         }
         public static String reverseSentence(String text)
         {
             return text;
         }
-        public static String[] splitWords(String text)
+        public static String[] SubArray(String[] data, int index, int length)
         {
-            return null;
+            String[] result = new String[length];
+            Array.Copy(data, index, result, 0, length);
+            return result;
         }
     }
 }
