31b76425d738e0100e6566788237e58e1520de36
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2019-10-09 13:05:29 +0200

    created methods and implemented reverseLetters

--- A01_Buchstabendreher/Program.cs
+++ A01_Buchstabendreher/Program.cs
@@ -6,8 +6,26 @@ namespace A01_Buchstabendreher
     {
         static void Main(string[] args)
         {
-
-
+            Console.WriteLine("Bitte einen kleinen Satz eingeben");
+            Console.Write("> ");
+            var text = Console.ReadLine();
+            string letters = reverseLetters(text);
+            string words = reverseWords(text);
+            string sentence = reverseSentence(text);
+            Console.WriteLine(sentence + "\n" + words + "\n" + letters);
+        }
+        //reverses the Letters of a String (shortest possible Implementation)
+        public static String reverseLetters(String text)
+        {
+            return (text.Length == 0)? "" : text[text.Length - 1] + reverseLetters(text.Substring(0, text.Length - 1));
+        }
+        public static String reverseWords(String text)
+        {
+            return text;
+        }
+        public static String reverseSentence(String text)
+        {
+            return text;
         }
     }
 }
