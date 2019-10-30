5e7030b6ea6f6badfc1f3f2b0dbe3e7a6cb5d48e
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2019-10-30 10:30:30 +0100

    added C# project for oxo

---
+++ A03_OXO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace A03_OXO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKey choice;
+            do
+            {
+            choice = Console.ReadKey(true).Key;
+                switch (choice)
+                {
+                    case ConsoleKey.NumPad1:
+                        Console.WriteLine("1. Choice");
+                        break;
+                    case ConsoleKey.NumPad2:
+                        Console.WriteLine("2. Choice");
+                        break;
+                }
+            } while (choice != ConsoleKey.D1 && choice != ConsoleKey.D2);
+        }
+    }
