26169cb2526b30a95dac55fe951c6f57c4b77ad8
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2019-11-12 13:31:06 +0100

    added numpad input to program

--- A04_Quiz/Program.cs
+++ A04_Quiz/Program.cs
@@ -8,27 +8,29 @@ namespace A04_Quiz
         static void Main(string[] args)
         {
             Console.Clear();
-            Quiz quiz = new Quiz(startingQuizelements());
-            String option;
+            Quiz quiz = new Quiz(StartingQuizelements());
+            Quizloop(quiz);
+        }
+        static void Quizloop(Quiz quiz){
+            ConsoleKey choice;
             do {
                 Console.Clear();
-                option = Console.ReadLine();
-                switch(option){
-                    case "1":
-                        //Dosomething
-                        break;
-                    case "2":
-                        //Dosomething
-                        break;
-                    case "3":
-                        //Dosomething
-                        break;
-                }
-            } while(option != "1");
-            Console.WriteLine("Stopped");
+                Console.WriteLine("Menu\n\n1. answer question\n2. create question\n3. exit\n\n press the number of your choise");
+                do{
+                    choice = Console.ReadKey(true).Key;
+                } while(choice != ConsoleKey.D1 && choice != ConsoleKey.D2 && choice != ConsoleKey.D3);
+                switch(choice)
+                    {
+                        case ConsoleKey.D1:
+                            quiz.AnswerQuestion();
+                            break;
+                        case ConsoleKey.D2:
+                            quiz.AddQuestion();
+                            break;
+                    }
+            } while(choice != ConsoleKey.D3);
         }
-
-        static List<Quizelement> startingQuizelements(){
+        static List<Quizelement> StartingQuizelements(){
             List<Quizelement> quizelements = new List<Quizelement>();
             quizelements.Add(new Quizelement("Ist Daniel böse?","ja",new String[] {"nein"}));
             return quizelements;
