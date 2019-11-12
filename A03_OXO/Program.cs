e0213d0d9dcdaaa548aedca12f30f420feec337d
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2019-11-12 10:18:19 +0100

    added program

--- A03_OXO/Program.cs
+++ A03_OXO/Program.cs
@@ -2,24 +2,191 @@
 
 namespace A03_OXO
 {
-    class Program
+    class OXO
     {
+        static string[,] board = new string[3, 3];
+        static int player;
+
         static void Main(string[] args)
         {
-            ConsoleKey choice;
-            do
+            Console.Clear();
+            for (int i = 0; i < 3; i++)
             {
-            choice = Console.ReadKey(true).Key;
-                switch (choice)
+                for (int j = 0; j < 3; j++)
                 {
-                    case ConsoleKey.D1:
-                        Console.WriteLine("1. Choice");
-                        break;
-                    case ConsoleKey.D2:
-                        Console.WriteLine("2. Choice");
-                        break;
+                    board[i, j] = "-";
                 }
-            } while (choice != ConsoleKey.D1 && choice != ConsoleKey.D2);
+            }
+            drawBoard();
+            player = 1;
+        
+            keyInput();
+        }
+
+        static void drawBoard()
+        {
+            Console.WriteLine(board[0, 0] + " " + board[0, 1] + " " + board[0, 2]);
+            Console.WriteLine(board[1, 0] + " " + board[1, 1] + " " + board[1, 2]);
+            Console.WriteLine(board[2, 0] + " " + board[2, 1] + " " + board[2, 2]);
+        }
+
+        static void keyInput()
+        {
+
+            ConsoleKey choice;
+            int x = 0;
+            int y = 0;
+
+            do{
+                choice = Console.ReadKey(true).Key;
+            } while(choice != ConsoleKey.D1 && choice != ConsoleKey.D2 && choice != ConsoleKey.D3 && choice != ConsoleKey.D4 && choice != ConsoleKey.D5 && choice != ConsoleKey.D6 && choice != ConsoleKey.D7 && choice != ConsoleKey.D8 && choice != ConsoleKey.D9);
+            
+            switch(choice)
+            {
+                case ConsoleKey.D1:
+                    x = 2;
+                    y = 0;
+                    break;
+                case ConsoleKey.D2:
+                    x = 2;
+                    y = 1;
+                    break;
+                case ConsoleKey.D3:
+                    x = 2;
+                    y = 2;
+                    break;
+                case ConsoleKey.D4:
+                    x = 1;
+                    y = 0;
+                    break;
+                case ConsoleKey.D5:
+                    x = 1;
+                    y = 1;
+                    break;
+                case ConsoleKey.D6:
+                    x = 1;
+                    y = 2;
+                    break;
+                case ConsoleKey.D7:
+                    x = 0;
+                    y = 0;
+                    break;
+                case ConsoleKey.D8:
+                    x = 0;
+                    y = 1;
+                    break;
+                case ConsoleKey.D9:
+                    x = 0;
+                    y = 2;
+                    break;
+            }
+
+            if (board[x, y] == "-")
+                {
+                    Console.Clear();
+                    if (player == 1)
+                    {
+                        board[x, y] = "X";
+                    }
+                    else if (player == 2)
+                    {
+                        board[x, y] = "O";
+                    }
+
+                    drawBoard();
+                    if(checkWinConditions(x, y))
+                    {
+                        displayWinMessage(player);
+                        return;
+                    }
+                    if(checkIfBoardIsFull()) {
+                        displayDrawMessage();
+                        return;
+                    }
+
+                    if(player == 1)
+                    {
+                        player = 2;
+                    }
+                    else
+                    {
+                        player = 1;
+                    }
+                    keyInput();
+                }
+                else
+                {
+                    keyInput();
+                }
+        }
+
+        static void invalidInput()
+        {
+            Console.WriteLine("Ungültige Eingabe. Bitte versuche es erneut.");
+        }
+
+        static bool checkWinConditions(int row, int column)
+        {
+            string p;
+            if(player == 1)
+            {
+                p = "X";
+            }
+            else
+            {
+                p = "O";
+            }
+
+            if(board[row, 0] == p && board[row, 1] == p && board[row, 2] == p) 
+            {
+                return true;
+            }
+            else if(board[0, column] == p && board[1, column] == p && board[2, 1] == p)
+            {
+                return true;
+            }
+            else if(board[0, 0] == p && board[1, 1] == p && board[2, 2] == p)
+            {
+                return true;
+            }
+            else if(board[0, 2] == p && board[1,1] == p && board[2,0] == p)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static bool checkIfBoardIsFull() {
+
+            int counter = 0;
+
+            foreach(string s in board)
+            {
+                if(s == "-")
+                    counter += 1;
+            }
+
+            if(counter > 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        static void displayWinMessage(int player)
+        {
+            Console.WriteLine("Spieler " + player + " hat gewonnen!");
+        }
+
+        static void displayDrawMessage()
+        {
+            Console.WriteLine("Unentschieden.");
         }
     }
 }
