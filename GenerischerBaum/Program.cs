62c66542a6c6eca695bd42a371b316c7400e53f2
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2019-12-03 15:13:35 +0100

    created treenode class

---
+++ GenerischerBaum/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace GenerischerBaum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode<int> root = new TreeNode<int>(null, 0);
+        }
+    }
+}
---
+++ GenerischerBaum/tree/TreeNode.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+
+namespace GenerischerBaum
+{
+    class TreeNode<T>
+    {
+        private TreeNode<T> parentNode;
+        private List<TreeNode<T>> childNodes;
+        private T content;
+        public TreeNode(TreeNode<T> parentNode, T content)
+        {
+            this.parentNode = parentNode;
+            this.childNodes = new List<TreeNode<T>>();
+            this.content = content;
+        }
+        /*The method CreateNode returns a new treenode 
+        containing the given content*/
+        public TreeNode<T> CreateNode(T content){
+            return new TreeNode<T>(null, content);
+        }
+        /*The method CreateChildNode creates a childnode with content*/
+        public void CreateChildNode(T content){
+            childNodes.Add(new TreeNode<T>(this, content));
+        }
+        /*The method IsNodeInBranch searches for a treenode in 
+        and under the treenode which executes the method(it does
+        matter which node executes the method)*/
+        public bool IsNodeInBranch(TreeNode<T> searchNode){
+            bool isBelow = false;
+            if(this == searchNode){
+                isBelow = true;
+            }
+            int index = 0;
+            while((isBelow == false) && (index < this.childNodes.Count)){
+                isBelow = childNodes[index].IsNodeInBranch(searchNode);
+                index++;
+            }
+            return isBelow;
+        }
+        /*The method IsNodeInTree searches for a treenode in 
+        and under the complete tree (it does not matter which node 
+        of the tree executes the method)*/
+        public bool IsNodeInTree(TreeNode<T> searchNode){
+            TreeNode<T> root = GetTreeRoot();
+            return root.IsNodeInBranch(searchNode);;
+        }
+        /*The method GetTreeRoot returns the rootnode of the tree*/
+        public TreeNode<T> GetTreeRoot(){
+            return (this.parentNode != null)?  this.parentNode.GetTreeRoot() : this;
+        }
+        /*The method SetParent sets the parentNode to the given parent*/
+        public void SetParent(TreeNode<T> parentNode){
+            this.parentNode = parentNode;
+        }
+        /*The method AppandChild adds a childnode to the node*/
+        public void AppandChild(TreeNode<T> childNode){
+            if(IsNodeInTree(childNode)){
+                throw new System.ArgumentException("the childnode is already in the tree");
+            } else {
+                childNode.SetParent(this); //secures that the parent is correctly set
+                this.childNodes.Add(childNode);
+            }
+        }
+        /*The method ToString returns a string 
+        containing the info of the content*/
+        public override string ToString(){
+            return content.ToString();
+        }
+        /*The method PrintTree prints the complete tree to the Console*/
+        public void PrintTree(){
+            Console.WriteLine(GetTreeRoot().GetBranchInformation());
+        }
+        /*The method GetBranchInformation returns 
+        the content and the structure of the branch*/
+        private string GetBranchInformation(){
+            return ToString();
+        }
+
+        public void ForEach(){
+
+        }
+    }
+}
