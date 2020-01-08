b10eb613e5cd94abb834bebac3ae5ed83431ab0f
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2020-01-08 17:52:59 +0100

    added use case diagram - first draft

---
+++ Abschlussaufgabe/TextAdventure/TextAdventureGraph/Graph.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace TextAdventureGraph
+{
+    public class GraphElement
+    {
+        private List<GraphElement> Associations;
+    }
+
+}
