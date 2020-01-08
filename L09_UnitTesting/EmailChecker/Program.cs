4fae6bfd4641b6b97425e484ca880c52c6f83303
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2020-01-08 10:24:22 +0100

    added test code

---
+++ L09_UnitTesting/EmailChecker/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EmailChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1();
+            System.Threading.Thread.Sleep(10000);
+        }
+
+        public static void Test1(){
+            string mailaddress = "ich@provider.com";
+            if (IsEmailAddress(mailaddress))
+                Console.WriteLine("TEST PASSED: " + mailaddress + " korrekt als Email-Adresse erkannt");
+            else
+                Console.WriteLine("TEST FAILED: " + mailaddress + " nicht als Email-Adresse erkannt, obwohl korrekt.");
+        }
+        public static bool IsEmailAddress(string emailAddress)
+        {
+            int iAt = emailAddress.IndexOf('@');
+            int iDot = emailAddress.LastIndexOf('.');
+            return (iAt > 0 && iDot > iAt);
+        }
+    }
+}
