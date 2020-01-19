8751db9992e3768ab8bedd6ec6a3e8dc06ec764c
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2020-01-19 19:40:07 +0100

    compiled for first time

--- Abschlussaufgabe/TextAdventure/Program.cs
+++ Abschlussaufgabe/TextAdventure/Program.cs
@@ -6,6 +6,7 @@ namespace TextAdventure
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Game is starting...");
             // TODO: execute Game
         }
     }
