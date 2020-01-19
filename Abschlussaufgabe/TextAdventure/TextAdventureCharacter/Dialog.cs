5ed7a5b53d48479c1a638fc10c331861f9cd954b
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2020-01-19 19:37:53 +0100

    created classes for map

---
+++ Abschlussaufgabe/TextAdventure/TextAdventureCharacter/Dialog.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace TextAdventureCharacter
+{
+    public sealed class Dialog
+    {
+    }
+}
---
+++ Abschlussaufgabe/TextAdventure/TextAdventureMap/Gateway.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace TextAdventureMap
+{
+    public class Gateway
+    {
+    }
+}
