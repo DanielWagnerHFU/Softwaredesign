bb1d247552dff02a3e1227c067628af91901a652
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2020-02-04 00:23:28 +0100

    tests

--- Abschlussaufgabe/TextAdventure.Test/UnitTest1.cs
+++ Abschlussaufgabe/TextAdventure.Test/UnitTest1.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using TextAdventureCharacter;
+using System.Linq;
 
 namespace TextAdventure.Test
 {
@@ -47,6 +48,22 @@ namespace TextAdventure.Test
             Command command = new Command(new string[]{"te"},new ParameterMethod<string>(TestDelegate),"");
             bool isEqual = command.IsEqualToCommandWithArgs("test");
             Assert.False(isEqual, "Fehler beim Vergleich der Befehle mit und ohne Argumente");
+        }
+        [Fact]
+        public void Test7()
+        {
+            Command command = new Command(new string[]{"test"},new ParameterMethod<string>(TestDelegate),"");
+            string[] args = command.GetArgs("test 1 2 ab 3");
+            string[] testargs = {"1","2","ab","3"};
+            Assert.True(args.SequenceEqual(testargs), testargs[0]+args[0]+" "+testargs[1]+args[1]+" "+testargs[2]+args[2]+" "+testargs[3]+args[3]+" ");
+        }
+        [Fact]
+        public void Test8()
+        {
+            Command command = new Command(new string[]{"test"},new ParameterMethod<string>(TestDelegate),"");
+            string[] args = command.GetArgs("test");
+            string[] testargs = new string[0];
+            Assert.True(args.SequenceEqual(testargs), testargs[0]+args[0]+" "+testargs[1]+args[1]+" "+testargs[2]+args[2]+" "+testargs[3]+args[3]+" ");
         }
         private void TestDelegate(string[] args)
         {
