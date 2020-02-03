96d48fbb4ad9120d0ce22db1a1c08f5e0ec77872
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2020-02-03 23:17:49 +0100

    modified test

--- Abschlussaufgabe/TextAdventure.Test/UnitTest1.cs
+++ Abschlussaufgabe/TextAdventure.Test/UnitTest1.cs
@@ -10,7 +10,8 @@ namespace TextAdventure.Test
         public void Test1()
         {
             Command command = new Command(new string[]{"test"},new ParameterMethod<string>(TestDelegate),"");
-
+            bool isEqual = command.IsEqualToCommandWithArgs("test 1 2 3");
+            Assert.True(isEqual, "Fehler beim Vergleich der Befehle mit und ohne Argumente");
         }
         private void TestDelegate(string[] args)
         {
