f1fd96a8ad1b58d4e3e5a198a0508573b990843e
DanielWagnerHFU <45201769+DanielWagnerHFU@users.noreply.github.com>
2020-02-09 14:18:53 +0100

    builded and changed uin to show on gateways

--- Abschlussaufgabe/TextAdventure/TextAdventureMap/Door.cs
+++ Abschlussaufgabe/TextAdventure/TextAdventureMap/Door.cs
@@ -12,8 +12,8 @@ namespace TextAdventureMap
         private int keyHole;
         private string name;
         private bool isOpen;
-        public Door(Area areaA, Area areaB, string name, int keyHole, bool isOpen) 
-        : base(areaA, areaB)
+        public Door(int uniqueIdentificationNumber, Area areaA, Area areaB, string name, int keyHole, bool isOpen) 
+        : base(uniqueIdentificationNumber, areaA, areaB)
         {
             this.keyHole = keyHole;
             this.name = name;
@@ -22,15 +22,19 @@ namespace TextAdventureMap
         public override string GetDescription(Area callingArea){
             if(isOpen == true)
             {
-                return this.name + " open to " + GetDestinationName(callingArea);
+                return this.name + " open to " + GetDestinationName(callingArea) + " [" + callingArea.GetUIN() + "]";
             } 
             else 
             {
-                return "closed " + this.name + " to " + GetDestinationName(callingArea);
+                return "closed " + this.name + " [" + this.uniqueIdentificationNumber + "]";
             }
         }
         public override string GetName(Area callingArea){
             return this.name;
+        }
+        public override int GetUIN(Area callingArea)
+        {
+            return this.uniqueIdentificationNumber;
         }
         public string GetName()
         {
@@ -62,14 +66,15 @@ namespace TextAdventureMap
         }
         new public static Door BuildFromXmlNode(XmlNode gatewayNode, List<Area> areaList){
             XmlAttributeCollection attributes = gatewayNode.Attributes;
-            int uinAreaA = Int32.Parse(attributes[1].Value);
-            int uinAreaB = Int32.Parse(attributes[2].Value);
+            int uin = Int32.Parse(attributes[1].Value);
+            int uinAreaA = Int32.Parse(attributes[2].Value);
+            int uinAreaB = Int32.Parse(attributes[3].Value);
             Area areaA = areaList.Find(area => area.GetUIN() == uinAreaA);
             Area areaB = areaList.Find(area => area.GetUIN() == uinAreaB);
-            string name = attributes[3].Value;
-            int keyHole = Int32.Parse(attributes[4].Value);
-            bool isOpen = Boolean.Parse(attributes[5].Value);
-            return new Door(areaA, areaB, name, keyHole, isOpen);
+            string name = attributes[4].Value;
+            int keyHole = Int32.Parse(attributes[5].Value);
+            bool isOpen = Boolean.Parse(attributes[6].Value);
+            return new Door(uin, areaA, areaB, name, keyHole, isOpen);
         }
     }
 }
--- Abschlussaufgabe/TextAdventure/TextAdventureMap/Gateway.cs
+++ Abschlussaufgabe/TextAdventure/TextAdventureMap/Gateway.cs
@@ -9,19 +9,25 @@ namespace TextAdventureMap
 {
     public class Gateway
     {
+        protected int uniqueIdentificationNumber;
         protected Area areaA;
         protected Area areaB;
-        public Gateway(Area areaA, Area areaB)
+        public Gateway(int uniqueIdentificationNumber, Area areaA, Area areaB)
         {
             this.areaA = areaA;
             areaA.AddGateway(this);
             this.areaB = areaB;
             areaB.AddGateway(this);
+            this.uniqueIdentificationNumber = uniqueIdentificationNumber;
         }
         public virtual void ChangeArea(Character character)
         {
             character.MoveToArea(GetDestination(character.GetLocation()));
             character.SetIsOnMove(false);
+        }
+        public virtual int GetUIN(Area callingArea)
+        {
+            return (callingArea == areaA)? areaB.GetUIN() : areaA.GetUIN();
         }
         public virtual string GetName(Area callingArea)
         {
@@ -29,7 +35,7 @@ namespace TextAdventureMap
         }
         public virtual string GetDescription(Area callingArea)
         {
-            return GetName(callingArea);
+            return GetName(callingArea) + " [" + callingArea.GetUIN() + "]";
         }
         public string GetDestinationName(Area callingArea)
         {
@@ -41,11 +47,12 @@ namespace TextAdventureMap
         }
         public static Gateway BuildFromXmlNode(XmlNode gatewayNode, List<Area> areaList){
             XmlAttributeCollection attributes = gatewayNode.Attributes;
-            int uinAreaA = Int32.Parse(attributes[1].Value);
-            int uinAreaB = Int32.Parse(attributes[2].Value);
+            int uin = Int32.Parse(attributes[1].Value);
+            int uinAreaA = Int32.Parse(attributes[2].Value);
+            int uinAreaB = Int32.Parse(attributes[3].Value);
             Area areaA = areaList.Find(area => area.GetUIN() == uinAreaA);
             Area areaB = areaList.Find(area => area.GetUIN() == uinAreaB);
-            return new Gateway(areaA, areaB);
+            return new Gateway(uin, areaA, areaB);
         }
     }
 }
